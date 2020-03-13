b748a061456240dfe8764bc2637786e8707c63a9
xiaojian <xiaojian00007@163.com>
2020-03-13 22:51:05 +0800

    添加去除空格方法和注释

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # ininin-validator
+
 表单验证工具类，包含整数验证、小数验证、电话号码验证
 
 # install
+
 npm i ininin-validator
 
 # github
+
 https://github.com/yy-ininin/ininin-validator
 
 # usage
+
 ```javascript
-import validator from 'ininin-validator'
+import validator from "ininin-validator";
 
 /**
  * 验证整数
@@ -22,8 +26,12 @@ import validator from 'ininin-validator'
  * @param {Boolean} [opts.thanEqual] 是否为开区间（大于等于/小于等于），默认true
  * @returns {String} 验证信息
  */
-const validMsg = validator.validInteger({value: 0, field: '年龄', minimum: 1})
-console.log(validMsg)
+const validMsg = validator.validInteger({
+  value: 0,
+  field: "年龄",
+  minimum: 1
+});
+console.log(validMsg);
 // 年龄必须大于等于 1
 
 /**
@@ -38,8 +46,8 @@ console.log(validMsg)
  * @param {Boolean} [opts.thanEqual] 是否为开区间（大于等于/小于等于），默认true
  * @returns {String} 验证信息
  */
-const validMsg = validator.validDecimal({value: 0.455, field: '金额'})
-console.log(validMsg)
+const validMsg = validator.validDecimal({ value: 0.455, field: "金额" });
+console.log(validMsg);
 // 金额仅支持两位小数
 
 /**
@@ -51,7 +59,26 @@ console.log(validMsg)
  * @param {Boolean} [opts.required] 是否必填，默认false
  * @returns {String} 验证信息
  */
-const validMsg = validator.validPhone({value: '1281234567', field: '手机号'})
-console.log(validMsg)
+const validMsg = validator.validPhone({ value: "1281234567", field: "手机号" });
+console.log(validMsg);
 // 手机号格式有误
-```
+
+/**
+ * 去除空格
+ * @param  {str}  str 字符串 必填
+ * @param  {type}  type:  1-所有空格  2-前后空格  3-前空格 4-后空格
+ * @return {String} 字符串
+ */
+let str1 = " A A "
+validator.trim(str1, 1)
+// "AA"
+
+validator.trim(str1, 2)
+// "A A"
+
+validator.trim(str1, 3)
+// "A A "
+
+validator.trim(str1, 4)
+// " A A"
+
--- index.js
+++ index.js
@@ -6,8 +6,34 @@
  * @version 1.1
  * @since 2016/2/22
  * @constructor
+ *
+ * @author shlijian@ininin.com
+ * @version 1.1.1
+ * @since 2020/3/13
+ * @description 去除空格
  */
 module.exports = {
+    /**
+     * 去除空格
+     * @param  {str}  str 字符串 必填
+     * @param  {type}  type:  1-所有空格  2-前后空格  3-前空格 4-后空格
+     * @return {String} 字符串
+     */
+    trim(str, type) {
+        type = type || 1;
+        switch (type) {
+            case 1:
+                return str.replace(/\s+/g, "");
+            case 2:
+                return str.replace(/(^\s*)|(\s*$)/g, "");
+            case 3:
+                return str.replace(/(^\s*)/g, "");
+            case 4:
+                return str.replace(/(\s*$)/g, "");
+            default:
+                return str;
+        }
+    },
     /**
      * 验证整数
      * @param {Object} opts 参数
@@ -20,35 +46,35 @@ module.exports = {
      * @returns {String} 验证信息
      */
     validInteger(opts) {
-        opts = opts || {}
-        opts.field = opts.field == null ? '' : opts.field
-        opts.required = opts.required == null ? false : opts.required
-        opts.thanEqual = opts.thanEqual == null ? true : opts.thanEqual
-        let msg = ''
-        let val = Number(opts.value)
-        let str = String(opts.value)
-        let minimum = opts.minimum == null ? -999999999 : opts.minimum
-        let maximum = opts.maximum == null ? 999999999 : opts.maximum
-        if (opts.value === '') {
-            msg = opts.required ? `请填写${opts.field}` : ''
+        opts = opts || {};
+        opts.field = opts.field == null ? "" : opts.field;
+        opts.required = opts.required == null ? false : opts.required;
+        opts.thanEqual = opts.thanEqual == null ? true : opts.thanEqual;
+        let msg = "";
+        let val = Number(opts.value);
+        let str = String(opts.value);
+        let minimum = opts.minimum == null ? -999999999 : opts.minimum;
+        let maximum = opts.maximum == null ? 999999999 : opts.maximum;
+        if (opts.value === "") {
+            msg = opts.required ? `请填写${opts.field}` : "";
         } else if (val < minimum && opts.thanEqual) {
-            msg = `${opts.field}必须大于等于 ${minimum}`
+            msg = `${opts.field}必须大于等于 ${minimum}`;
         } else if (val <= minimum && !opts.thanEqual) {
-            msg = `${opts.field}必须大于 ${minimum}`
+            msg = `${opts.field}必须大于 ${minimum}`;
         } else if (val > maximum && opts.thanEqual) {
-            msg = `${opts.field}必须小于等于 ${maximum}`
+            msg = `${opts.field}必须小于等于 ${maximum}`;
         } else if (val >= maximum && !opts.thanEqual) {
-            msg = `${opts.field}必须小于 ${maximum}`
+            msg = `${opts.field}必须小于 ${maximum}`;
         } else if (/^0\d+/.test(str)) {
-            msg = `${opts.field}不能以 0 开头`
+            msg = `${opts.field}不能以 0 开头`;
         } else if (/\.\d+$/.test(str)) {
-            msg = `${opts.field}只能是整数`
+            msg = `${opts.field}只能是整数`;
         } else if (/^\d+$/.test(str)) {
-            msg = ''
+            msg = "";
         } else {
-            msg = `请填写正确的${opts.field}`
+            msg = `请填写正确的${opts.field}`;
         }
-        return msg
+        return msg;
     },
     /**
      * 验证小数
@@ -63,14 +89,14 @@ module.exports = {
      * @returns {String} 验证信息
      */
     validDecimal(opts) {
-        opts = opts || {}
-        opts.field = opts.field == null ? '' : opts.field
-        opts.decimals = opts.decimals == null ? 2 : opts.decimals
-        opts.required = opts.required == null ? false : opts.required
-        opts.thanEqual = opts.thanEqual == null ? true : opts.thanEqual
-        let msg = ''
-        let val = Number(opts.value)
-        let str = String(opts.value).replace(/^-/, '')
+        opts = opts || {};
+        opts.field = opts.field == null ? "" : opts.field;
+        opts.decimals = opts.decimals == null ? 2 : opts.decimals;
+        opts.required = opts.required == null ? false : opts.required;
+        opts.thanEqual = opts.thanEqual == null ? true : opts.thanEqual;
+        let msg = "";
+        let val = Number(opts.value);
+        let str = String(opts.value).replace(/^-/, "");
         let regObj = {
             1: /^\d+(\.\d?)?$/,
             2: /^\d+(\.\d{0,2})?$/,
@@ -78,42 +104,42 @@ module.exports = {
             4: /^\d+(\.\d{0,4})?$/,
             5: /^\d+(\.\d{0,5})?$/,
             6: /^\d+(\.\d{0,6})?$/
-        }
-        let reg = regObj[opts.decimals] || regObj[2]
-        let minimum = opts.minimum == null ? -999999999 : opts.minimum
-        let maximum = opts.maximum == null ? 999999999 : opts.maximum
-        if (opts.value === '') {
-            msg = opts.required ? `请填写${opts.field}` : ''
+        };
+        let reg = regObj[opts.decimals] || regObj[2];
+        let minimum = opts.minimum == null ? -999999999 : opts.minimum;
+        let maximum = opts.maximum == null ? 999999999 : opts.maximum;
+        if (opts.value === "") {
+            msg = opts.required ? `请填写${opts.field}` : "";
         } else if (val < minimum && opts.thanEqual) {
-            msg = `${opts.field}必须大于等于 ${minimum}`
+            msg = `${opts.field}必须大于等于 ${minimum}`;
         } else if (val <= minimum && !opts.thanEqual) {
-            msg = `${opts.field}必须大于 ${minimum}`
+            msg = `${opts.field}必须大于 ${minimum}`;
         } else if (val > maximum && opts.thanEqual) {
-            msg = `${opts.field}必须小于等于 ${maximum}`
+            msg = `${opts.field}必须小于等于 ${maximum}`;
         } else if (val >= maximum && !opts.thanEqual) {
-            msg = `${opts.field}必须小于 ${maximum}`
+            msg = `${opts.field}必须小于 ${maximum}`;
         } else if (/^0\d+/.test(str)) {
-            msg = `${opts.field}不能以 0 开头`
+            msg = `${opts.field}不能以 0 开头`;
         } else {
             if (opts.decimals == 1 && /\.\d{2,}$/.test(str)) {
-                msg = `${opts.field}仅支持一位小数`
+                msg = `${opts.field}仅支持一位小数`;
             } else if (opts.decimals == 2 && /\.\d{3,}$/.test(str)) {
-                msg = `${opts.field}仅支持两位小数`
+                msg = `${opts.field}仅支持两位小数`;
             } else if (opts.decimals == 3 && /\.\d{4,}$/.test(str)) {
-                msg = `${opts.field}仅支持三位小数`
+                msg = `${opts.field}仅支持三位小数`;
             } else if (opts.decimals == 4 && /\.\d{5,}$/.test(str)) {
-                msg = `${opts.field}仅支持四位小数`
+                msg = `${opts.field}仅支持四位小数`;
             } else if (opts.decimals == 5 && /\.\d{6,}$/.test(str)) {
-                msg = `${opts.field}仅支持五位小数`
+                msg = `${opts.field}仅支持五位小数`;
             } else if (opts.decimals == 6 && /\.\d{7,}$/.test(str)) {
-                msg = `${opts.field}仅支持六位小数`
+                msg = `${opts.field}仅支持六位小数`;
             } else if (reg.test(str)) {
-                msg = ''
+                msg = "";
             } else {
-                msg = `请填写正确的${opts.field}`
+                msg = `请填写正确的${opts.field}`;
             }
         }
-        return msg
+        return msg;
     },
     /**
      * 验证电话号码
@@ -125,44 +151,46 @@ module.exports = {
      * @returns {String} 验证信息
      */
     validPhone(opts) {
-        opts = opts || {}
-        opts.field = opts.field == null ? '' : opts.field
-        opts.mode = opts.mode == null ? 0 : opts.mode
-        opts.required = opts.required == null ? false : opts.required
-        let msg = ''
-        let str = String(opts.value).replace(/^-/, '')
-        const regMobile = /^1[3|4|5|6|7|8|9]\d{9}$/
-        const regTel = /^0\d{2,3}-?\d{7,8}$/
+        opts = opts || {};
+        opts.field = opts.field == null ? "" : opts.field;
+        opts.mode = opts.mode == null ? 0 : opts.mode;
+        opts.required = opts.required == null ? false : opts.required;
+        let msg = "";
+        let str = String(opts.value).replace(/^-/, "");
+        const regMobile = /^1[3|4|5|6|7|8|9]\d{9}$/;
+        const regTel = /^0\d{2,3}-?\d{7,8}$/;
         ///^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-        if (opts.value === '') {
-            msg = opts.required ? `请填写${opts.field}` : ''
-        } else if (opts.mode == 1) { //手机号
+        if (opts.value === "") {
+            msg = opts.required ? `请填写${opts.field}` : "";
+        } else if (opts.mode == 1) {
+            //手机号
             if (str.length < 11) {
-                msg = `${opts.field}必须是 11 位数字`
+                msg = `${opts.field}必须是 11 位数字`;
             } else if (/^\d+$/.test(str)) {
-                msg = `${opts.field}必须是 11 位数字`
+                msg = `${opts.field}必须是 11 位数字`;
             } else if (regMobile.test(str)) {
-                msg = ''
+                msg = "";
             } else {
-                msg = `${opts.field}格式有误`
+                msg = `${opts.field}格式有误`;
             }
-        } else if (opts.mode == 2) { //座机号
+        } else if (opts.mode == 2) {
+            //座机号
             if (str.length < 10) {
-                msg = `${opts.field}必须是 10~13 位`
+                msg = `${opts.field}必须是 10~13 位`;
             } else if (regTel.test(str)) {
-                msg = ''
+                msg = "";
             } else {
-                msg = `${opts.field}格式有误`
+                msg = `${opts.field}格式有误`;
             }
         } else {
             if (str.length < 10) {
-                msg = `${opts.field}必须是 10~13 位`
+                msg = `${opts.field}必须是 10~13 位`;
             } else if (regMobile.test(str) || regTel.test(str)) {
-                msg = ''
+                msg = "";
             } else {
-                msg = `${opts.field}格式有误`
+                msg = `${opts.field}格式有误`;
             }
         }
-        return msg
+        return msg;
     }
 };
